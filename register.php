a4261a3c2d4a19693b050e1ecd9c88b9c4f0c269
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2023-02-16 23:14:23 -0300

    register file OK

--- register.php
+++ register.php
@@ -1,1 +1,21 @@
 <?php
+
+require('conexion.php');
+
+$temperatura = $_GET['temperatura'];
+$progresiva = $_GET['progresiva'];
+$fecha = date("Y-m-d");
+$hora = date("H:i:s");
+
+// Prepare
+$stmt = $dbcon->prepare("INSERT INTO temperaturas (temp_grados, temp_progresiva, temp_fecha, temp_hora) VALUES (?, ?, ?, ?)");
+// Bind
+$stmt->bindParam(1, $temperatura);
+$stmt->bindParam(2, $progresiva);
+$stmt->bindParam(3, $fecha);
+$stmt->bindParam(4, $hora);
+// Excecute
+$stmt->execute();
+
+
+?>
